22f643ae25010c1a1c08df20de9a3bdde74fa6f3
theblackcat102 <13172147+theblackcat102@users.noreply.github.com>
2025-02-23 08:14:21 +0000

    [fix] handle breeze2 text mode

--- llms/breeze2.py
+++ llms/breeze2.py
@@ -14,7 +14,34 @@ from transformers import AutoModel, AutoTokenizer, GenerationConfig
 import torch
 import tempfile
 from PIL import Image
+from mtkresearch.llm.image_process_v3 import load_image
 from mtkresearch.llm.prompt import MRPromptV3
+
+class MRPomptV3Patch(MRPromptV3):
+    def _get_content_from_list(self, content_list):
+        content = ''
+        pixel_values = None
+        for x in content_list:
+            if x['type'] == 'text':
+                content += x['text']
+            elif x['type'] == 'image':
+                if isinstance(x['image_path'], str):
+                    image = Image.open(x['image_path']).convert('RGB')
+                elif isinstance(x['image_path'], Image):
+                    image = x['image_path'].convert('RGB')
+
+                pixel_values = load_image(image)
+                width, height = image.size
+                image_content_token_num = self._get_image_content_token_num(width, height)
+                image_content_str = ''.join([self.image_content_token] * image_content_token_num)
+                content += f'{self.image_start_token}{image_content_str}{self.image_end_token}\n'
+            elif x['type'] == 'bbox':
+                bboxes = [self._normalize_bbox(box, x["width"], x["height"]) for box in x["coords"]]
+                content += f'{self.bbox_start_token}{repr(bboxes)}{self.bbox_end_token}'
+            else:
+                raise ValueError('unknown type in the content')
+        return content, pixel_values
+
 
 
 class Breeze2:
@@ -29,7 +56,7 @@ class Breeze2:
         ).eval()
 
         self.tokenizer = AutoTokenizer.from_pretrained(model_id, trust_remote_code=True, use_fast=False)
-        self.prompt_engine = MRPromptV3()
+        self.prompt_engine = MRPomptV3Patch()
         self.sys_prompt = (
             'You are a helpful AI assistant built by MediaTek Research. '
             'The user you are helping speaks Traditional Chinese and comes from Taiwan.'
@@ -73,17 +100,17 @@ class Breeze2:
             eos_token_id=128009,
             **kwargs
         )
-
+        has_image = False
         if image is not None:
             # Ensure the image is in RGB mode and save it to a temporary file.
-            image_path = self._save_temp_image(image.convert("RGB"))
             conversations = [
                 {"role": "system", "content": self.sys_prompt},
                 {"role": "user", "content": [
-                    {"type": "image", "image_path": image_path},
+                    {"type": "image", "image_path": image},
                     {"type": "text", "text": prompt}
                 ]}
             ]
+            has_image = True
         else:
             conversations = [
                 {"role": "system", "content": self.sys_prompt},
@@ -91,10 +118,14 @@ class Breeze2:
                     {"type": "text", "text": prompt}
                 ]}
             ]
-
-        prompt_str, pixel_values = self.prompt_engine.get_prompt(conversations)
-        output_str = self._inference(prompt_str, gen_config, pixel_values=pixel_values)
+        if has_image:
+            prompt_str, pixel_values = self.prompt_engine.get_prompt(conversations)
+            output_str = self._inference(prompt_str, gen_config, pixel_values=pixel_values)
+        else:
+            prompt_str = self.prompt_engine.get_prompt(conversations)
+            output_str = self._inference(prompt_str, gen_config)
         result = self.prompt_engine.parse_generated_str(output_str)
+
         # Count tokens for input and output.
         num_input_tokens = len(self.tokenizer(prompt_str).input_ids)
         num_output_tokens = len(self.tokenizer(result['content']).input_ids)
